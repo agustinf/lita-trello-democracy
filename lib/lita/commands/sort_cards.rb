c12826d46a14cd0095006c37c810a47986ac2bf6
Francisco Arevalo <farevalod@gmail.com>
2016-12-15 16:40:20 -0300

    style(): define scale for card scoring

--- lib/lita/commands/sort_cards.rb
+++ lib/lita/commands/sort_cards.rb
@@ -1,9 +1,10 @@
 class SortCards < PowerTypes::Command.new(:cards, :votes)
   def perform
+    SECONDS_IN_MONTH = 86400*30
     scored_cards = []
     @cards.each do |c|
       card_votes = @votes.select { |v| v.card_id == c.id }
-      score = card_votes.map { |v| v.score * Math.exp((1/86400*30*10)*(v.voted_at - Time.now)) }.inject(:+) / card_votes.size
+      score = card_votes.map { |v| v.score * Math.exp((v.voted_at - Time.now)/(SECONDS_IN_MONTH*10)) }.inject(:+) / card_votes.size
       scored_cards << {card: c, score:score}
     end
     scored_cards.sort { |x,y| x["score"] <=> y["score"] }.map(&:card)
