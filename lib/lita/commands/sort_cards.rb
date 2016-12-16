54cfcd920a5ea921c795e931bf8c2a7ad626a467
Felipe Balart <felbalart@gmail.com>
2016-12-16 13:11:19 -0300

    fix(sort_cards): handle card with no votes case

--- lib/lita/commands/sort_cards.rb
+++ lib/lita/commands/sort_cards.rb
@@ -3,9 +3,14 @@ class SortCards < PowerTypes::Command.new(:cards, :votes)
     scored_cards = []
     @cards.each do |c|
       card_votes = @votes.select { |v| v.card_id == c.id }
-      score = card_votes.map { |v| v.score * Math.exp((1/86400*30*10)*(v.voted_at - Time.now)) }.inject(:+) / card_votes.size
+      if card_votes.empty?
+        score = 0
+      else
+        score = card_votes.map { |v| v.score * Math.exp((1/86400*30*10)*(v.voted_at - Time.now)) }
+                          .inject(:+) / card_votes.size
+      end
       scored_cards << {card: c, score:score}
     end
-    scored_cards.sort { |x,y| x["score"] <=> y["score"] }.map(&:card)
+    scored_cards.sort { |x,y| y[:score] <=> x[:score] }.map { |sorted_card| sorted_card[:card] }
   end
 end
