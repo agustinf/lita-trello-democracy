b0a7229061bcf1aa74311c4791e08c9bc079a877
Felipe Balart <felbalart@gmail.com>
2016-12-16 13:09:58 -0300

    feat():  add dependencies

--- lib/lita-trello-democracy.rb
+++ lib/lita-trello-democracy.rb
@@ -9,9 +9,14 @@ Lita.load_locales Dir[File.expand_path(
 
 require "lita/commands/trello/base"
 require "lita/models/card"
+require "lita/models/vote"
 require "lita/commands/trello/get_cards"
 require "lita/commands/trello/sort_cards"
+require "lita/commands/sort_cards"
+require "lita/commands/sync_cards"
+require "lita/commands/parse_vote_response"
 require "lita/handlers/vote_handler"
+require "lita/services/voting_service"
 
 Lita::Handlers::VoteHandler.template_root File.expand_path(
   File.join("..", "..", "templates"), __FILE__
