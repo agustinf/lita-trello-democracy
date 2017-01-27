a3591414e6f07e7c16565ebf30ca0a9b0e558364
Guillermo Moreno <gmq@guillermomoreno.cl>
2017-01-27 13:23:56 -0300

    fix(): clean up sort

--- lib/lita/commands/sort_cards.rb
+++ lib/lita/commands/sort_cards.rb
@@ -1,10 +1,5 @@
 class SortCards < PowerTypes::Command.new(:cards)
   def perform
-    scored_cards = []
-    @cards.each do |c|
-      c.score = card.votes.empty? ? 0 : card.votes.map(&:score).inject(:+)
-      scored_cards << c
-    end
-    scored_cards.sort_by(&:score).reverse
+    @cards.sort_by(&:score).reverse
   end
 end
