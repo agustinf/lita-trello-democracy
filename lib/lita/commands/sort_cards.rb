b7ba0f497e114356ff95141d629308ff62161494
felbalart <felbalart@gmail.com>
2017-01-27 10:41:13 -0300

    feat(sort-cards): new score formula: simple vote adding

--- lib/lita/commands/sort_cards.rb
+++ lib/lita/commands/sort_cards.rb
@@ -3,13 +3,7 @@ class SortCards < PowerTypes::Command.new(:cards, :votes)
     scored_cards = []
     @cards.each do |c|
       card_votes = @votes.select { |v| v.card_id == c.id }
-      if card_votes.empty?
-        score = 0
-      else
-        score = card_votes.map { |v| v.score * Math.exp((1/86400*30*10)*(v.voted_at - Time.now)) }
-                          .inject(:+) / Math.sqrt(card_votes.size)
-      end
-      c.score = score
+      c.score = card_votes.empty? ? 0 : card_votes.map(&:score).inject(:+)
       c.votes = card_votes.count
       scored_cards << c
     end
